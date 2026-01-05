3b75809d89abf56df698a13c8de6a2a5b3b05526
Monika2229 <monikaalavelli818@gmail.com>
2026-01-05 23:35:37 +0530

    Spotlight effect project

---
+++ Projects/Spotlight effect project/script.js
@@ -0,0 +1,7 @@
+
+const box = document.querySelector(".box");
+
+box.addEventListener("mousemove", (e) => {
+    box.style.setProperty("--x", e.clientX + "px");
+    box.style.setProperty("--y", e.clientY + "px");
+});
